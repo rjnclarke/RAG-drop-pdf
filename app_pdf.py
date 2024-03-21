fba66cd49dc404f88a5cfcebc1c30d6617222543
Richard Clarke <rjnclarke@gmail.com>
2024-03-21 15:55:33 +0100

    app_pdf.py functioning on localhost

---
+++ app_pdf.py
@@ -0,0 +1,99 @@
+from langchain_openai import ChatOpenAI
+from langchain_openai import OpenAIEmbeddings
+from langchain.chains import RetrievalQA
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain.chains import ConversationalRetrievalChain
+from langchain.callbacks import StdOutCallbackHandler
+from langchain.vectorstores import FAISS
+from langchain.document_loaders import PyPDFLoader
+from langchain.memory import ChatMessageHistory, ConversationBufferMemory
+
+import chainlit as cl
+from chainlit.types import AskFileResponse
+
+from dotenv import load_dotenv
+import os 
+
+load_dotenv()
+
+os.environ["OPENAI_API_KEY"] = os.getenv("OPENAI_API_KEY")
+llm = ChatOpenAI(model='gpt-3.5-turbo')
+embeddings = OpenAIEmbeddings()
+text_splitter = RecursiveCharacterTextSplitter(chunk_size=500, chunk_overlap=50)
+
+def process_file(file):
+    Loader = PyPDFLoader
+
+    loader = Loader(file.path)
+    documents = loader.load()
+    chunked_docs = text_splitter.transform_documents(documents)
+    return chunked_docs
+
+@cl.on_chat_start
+async def start():
+    files = None
+
+    # Wait for the user to upload a file
+    while files == None:
+        files = await cl.AskFileMessage(
+            content="Please upload a text file to begin!", accept=["application/pdf"], max_size_mb = 4
+        ).send()
+
+    file = files[0]
+    msg = cl.Message(content=f"Processing `{file.name}`...", disable_feedback=True)
+    await msg.send()
+    docs = process_file(file)
+    vector_store = FAISS.from_documents(docs, embeddings)
+    
+    message_history = ChatMessageHistory()
+
+    memory = ConversationBufferMemory(
+        memory_key="chat_history",
+        output_key="answer",
+        chat_memory=message_history,
+        return_messages=True,
+    )
+
+    chain = ConversationalRetrievalChain.from_llm(
+        ChatOpenAI(model_name="gpt-3.5-turbo", temperature=0, streaming=True),
+        chain_type="stuff",
+        retriever=vector_store.as_retriever(),
+        memory=memory,
+        return_source_documents=True,
+    )
+
+    # Let the user know that the system is ready
+    msg.content = f"`{file.name}` processed. You can now ask questions!"
+    await msg.update()
+
+    cl.user_session.set("chain", chain)
+
+@cl.on_message
+async def on_message(message: cl.Message):
+    # getting db and retriever
+
+    chain = cl.user_session.get("chain")  # type: ConversationalRetrievalChain
+
+    cb = cl.AsyncLangchainCallbackHandler()
+    res = await chain.acall(message.content, callbacks=[cb])
+
+    answer = res["answer"]
+    source_documents = res["source_documents"]  # type: List[Document]
+
+    text_elements = []  # type: List[cl.Text]
+
+    if source_documents:
+        for source_idx, source_doc in enumerate(source_documents):
+            source_name = f"source_{source_idx}"
+            # Create the text element referenced in the message
+            text_elements.append(
+                cl.Text(content=source_doc.page_content, name=source_name)
+            )
+        source_names = [text_el.name for text_el in text_elements]
+
+        if source_names:
+            answer += f"\nSources: {', '.join(source_names)}"
+        else:
+            answer += "\nNo sources found"
+
+    await cl.Message(content=answer, elements=text_elements).send()
